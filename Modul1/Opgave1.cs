24b5b7b2ffd82cf676f601d7fb76e2d98d93c636
oleeriksen <oleeriksen1958@gmail.com>
2023-08-24 09:27:28 +0200

    init

---
+++ Modul1/Opgave1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace Modul1
+{
+    public class Opgave1
+    {
+        public void Run() {
+            Console.WriteLine("Her kommer løsningen af opgave 1");
+            string input = Console.ReadLine();
+            int tal = int.Parse(input);
+            for (int idx = 1; idx <= tal; idx++) {
+                Console.WriteLine(idx);
+            }
+        }
+    }
+}
+
---
+++ Modul1/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Modul1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        new Opgave1().Run();
+    }
+}
+
