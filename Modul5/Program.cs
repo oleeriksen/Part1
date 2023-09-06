d565fbcad0a7f530a40771e0e8b4f3077b41683f
oleeriksen <oleeriksen1958@gmail.com>
2023-09-06 12:26:54 +0200

    Modul4 og 5

---
+++ Modul5/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Modul5;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        List<string> names = new List<string>
+        {
+            "Bruce",
+            "Alfred",
+            "Tim",
+            "Richard"
+        };
+
+        names.ForEach(name => Console.WriteLine(name));
+
+        Console.WriteLine("[" + string.Join(",", names) + "]");
+
+        Console.WriteLine("Og dem med et e i...");
+        names = names.FindAll(name => name.Contains('e'));
+        names.ForEach(name => Console.WriteLine(name));
+    }
+}
+
