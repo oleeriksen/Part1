d0000b412f930b31b2dc497b8ea6c8c7139c5324
oleeriksen <oleeriksen1958@gmail.com>
2024-02-14 11:02:52 +0100

    modul4

---
+++ Modul4/Opgaver.cs
@@ -0,0 +1,41 @@
+﻿using System;
+namespace Modul4
+{
+    public class Opgaver
+    {
+
+        public void Run() {
+            int[] data = { 1, 3, 5,7 };
+            var avg = Average(data);
+            Console.WriteLine($"Gennemsnit er {avg}");
+        }
+
+        /// <summary>
+        /// Returnerer gennemsnittet af tal i a som en double.
+        /// Fejl hvis a er tom (længde på 0).
+        /// </summary>
+        double Average(int[] a) {
+            return 0;
+        }
+
+        /// <summary>
+        /// Returnerer gennemsnittet af tal i a som en double.
+        /// Fejl hvis a er tom (længde på 0). Undgår at beregne
+        /// summen af alle tallene - brug derfor denne, hvis der
+        /// er mange tal eller store tal
+        /// </summary>
+        double Average2(int[] a) {
+            double res = a[0];
+
+            for (int i=1; i < a.Length; i++)
+            {
+                double f = i;
+                res = (f / (f + 1)) * res + (1 / (f + 1)) * a[i];
+            }
+            return res;
+        }
+
+
+    }
+}
+
---
+++ Modul4/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace Modul4;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, Modul 4");
+        new Opgaver().Run();
+    }
+
+    static void f() {
+
+        string[] a = {"danske kroner", "euro", "pund(engelske"};
+
+
+        foreach (string valuta in a)
+        {
+            Console.WriteLine(valuta);
+        }
+
+    }
+
+    static void g() {
+
+        string[] a = new string[3];
+        a[0] = "danske kroner";
+        a[1] = "euro";
+        a[2] = "pund(engelske)";
+
+
+        for (int i = 0; i < a.Length; i++)
+        {
+            Console.WriteLine(a[i]);
+        }
+
+
+    }
+}
+
