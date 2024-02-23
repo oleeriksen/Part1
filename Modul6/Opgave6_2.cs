fa03d8f61ca3a94137261a1144e08489189b4798
oleeriksen <oleeriksen1958@gmail.com>
2024-02-23 13:31:09 +0100

    opg6-2

---
+++ Modul6/Opgave6_2.cs
@@ -0,0 +1,63 @@
+﻿using System;
+namespace Modul6
+{
+    public class Opgave6_2
+    {
+        public void Run()
+        {
+            Console.WriteLine("Indtast karaktere - afslut med -1:");
+            List<int> grades = new();
+            
+            int count = 0;
+            while (true)
+            {
+                int input =  ReadInt($"Hvad er karakter nr {count + 1}:");
+                if (input == -1)
+                    break;
+                count++;
+                grades.Add(input);
+            }
+            Console.WriteLine($"Gennemsnit: {Average(grades)}");
+
+            int[] theGrades = { -3, 0, 2, 4, 7, 10, 12 };
+
+            foreach (var aGrade in theGrades)
+            {
+                var c = Count(grades, aGrade);
+                if (c > 0)
+                    Console.WriteLine($"Antal af {aGrade} er {c}");
+            }
+        }
+
+        /// <summary>
+        /// Returnerer gennemsnittet af tal i a som en double.
+        /// Fejl hvis a er tom (længde på 0).
+        /// </summary>
+        double Average(List<int> a)
+        {
+            double sum = 0;
+            foreach (int value in a)
+                sum += value;
+            return sum / a.Count;
+        }
+
+
+        int ReadInt(string pretext = "")
+        {
+            Console.Write(pretext);
+            return int.Parse(Console.ReadLine());
+        }
+
+        /// <summary>
+        /// Return the number og [key] in [a]
+        /// </summary>
+        int Count(List<int> a, int key)
+        {
+            int res = 0;
+            foreach (var v in a)
+                if (key == v) res++;
+            return res;
+        }
+    }
+}
+
---
+++ Modul6/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Modul6;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello - Modul 6");
+
+
+        List<int> liste = new();
+
+        liste.RemoveAll((e) => e < 4);
+        new Opgave6_2().Run();
+    }
+}
+
